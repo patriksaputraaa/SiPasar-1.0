7c50eb11dc027e755899f2997cbdd30e294a7e4d
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-03-06 16:04:13 +0700

    Fixing Controller

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -104,7 +104,8 @@ $app->routeMiddleware([
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
-
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Reliese\Coders\CodersServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
