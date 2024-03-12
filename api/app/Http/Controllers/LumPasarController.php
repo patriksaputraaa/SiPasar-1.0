400e3a7a0f09cc51bd1ce66d80892831b6d42d1c
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-03-12 09:41:20 +0700

    Update LumPasarController.php

--- api/app/Http/Controllers/LumPasarController.php
+++ api/app/Http/Controllers/LumPasarController.php
@@ -9,23 +9,20 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class LumPasarController extends BaseController
 {
+    public function getlonlan(Request $request){
+        $pasar = Pasar::getLonLan();
+        return response()->json($pasar);
+    }
+
     public function index()
     {
-        // Logic to retrieve all Pasar records from the database
         $pasar = Pasar::all();
         return response()->json($pasar);
     }
 
     public function show($id)
     {
-        // Logic to retrieve a single Pasar record by ID
         $pasar = Pasar::findOrFail($id);
         return response()->json($pasar);
     }
-
-    public function getlonlan(Request $request){
-        $pasar = Pasar::getLonLan();
-        return response()->json($pasar);
-    }
-    
 }
